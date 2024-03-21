058a6bb11a831e93ea6e000ed51bd0e8422befec
YuJin Jeong <jjiny1127@gmail.com>
2024-03-21 20:00:59 +0900

    Script files working on slurm cluster (#4)

--- examples/asplos25/mpirun_pretrain_gpt_spiral.sh
+++ examples/asplos25/mpirun_pretrain_gpt_spiral.sh
@@ -1,82 +1,39 @@
 #!/bin/bash
 
-MPIRUN=/usr/local/bin/mpirun
-MPI_OPTIONS="-mca btl ^openib -mca pml ucx"
-MEGATRON_PATH=$HOME/asplos2025/Megatron-LM-mcrl
+#SBATCH -J spiral
+#SBATCH --mincpus=4
+#SBATCH --mem=0
+#SBATCH --exclusive
 
-# Change for your system
+if [ -n "${SLURM_JOB_ID:-}" ] ; then
+    SCRIPT_PATH=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')
+else
+    SCRIPT_PATH=$(realpath "$0")
+fi
 
-## conda
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate Megatron-cuda11.7
+# Configuration for custom env
+. $(dirname "${SCRIPT_PATH}")/config.sh
 
-## mpi
-NP=4
-GPUS_PER_NODE=4
-HOSTS="b4:${GPUS_PER_NODE}" # b3:2,b4:2
-
-## torch dist.
-export MASTER_ADDR="b4"
-export MASTER_PORT=6003
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-
-## Config file path
-MODEL_PATH=/home/n0/yujin/tmp/model/gpt-like-270m
-VOCAB_FILE=/home/n0/yujin/tmp/tokenizer/megatron/gpt2-vocab.json
-MERGE_FILE=/home/n0/yujin/tmp/tokenizer/megatron/gpt2-merges.txt
-
-DATASET_NAME=openwebtext
-DATASET_CONFIG=plan_text
-DATA_PATH=/data/z0/heehoon/openwebtext-mg/openwebtext_text_document
-
-# --spiral-stage-optimizer
-SPIRAL_ARGS="
+# Configuration for spiral training
+EXTRA_ARGS="
     --spiral \
     --spiral-remap \
-    --spiral-forward-virtual-size 2 \
-    --spiral-backward-virtual-size 3 \
+    --spiral-forward-virtual-size $SPIRAL_FWD \
+    --spiral-backward-virtual-size $SPIRAL_BWD \
     --spiral-recompute-activations \
-    --spiral-debug-backend \
+    --overlap-p2p-communication \
     --megatron-mpi
 "
 
-GPT_ARGS="
-    --tensor-model-parallel-size 1 \
-    --pipeline-model-parallel-size $NP \
-    --no-initialization \
-    --untie-embeddings-and-output-weights \
-    --distributed-backend nccl \
-    --overlap-p2p-communication \
-    --sequence-parallel \
-    --num-layers 24 \
-    --hidden-size 1024 \
-    --num-attention-heads 16 \
-    --seq-length 1024 \
-    --max-position-embeddings 1024 \
-    --micro-batch-size 1 \
-    --global-batch-size 4 \
-    --lr 0.00015 \
-    --train-iters 3 \
-    --eval-iters 0 \
-    --lr-decay-iters 320000 \
-    --lr-decay-style cosine \
-    --min-lr 1.0e-5 \
-    --weight-decay 1e-2 \
-    --lr-warmup-fraction .01 \
-    --clip-grad 0.0 \
-    --attention-dropout 0.0 \
-    --hidden-dropout 0.0 \
-    --no-gradient-accumulation-fusion
-"
+if [ ${SPIRAL_STAGED_OPTIMIZER} == "YES" ]; then
+    EXTRA_ARGS+=" --spiral-stage-optimizer"
+fi
 
-DATA_ARGS="
-    --data-path $DATA_PATH \
-    --vocab-file $VOCAB_FILE \
-    --merge-file $MERGE_FILE \
-    --data-impl mmap \
-    --split 949,50,1
-"
+if [ ${SPIRAL_DEBUG_BACKEND} == "YES" ]; then
+    EXTRA_ARGS+=" --spiral-debug-backend"
+fi
 
-${MPIRUN} -np $NP -host $HOSTS $MPI_OPTIONS \
-    python $MEGATRON_PATH/pretrain_gpt.py $SPIRAL_ARGS $GPT_ARGS $DATA_ARGS --load $MODEL_PATH
+# Run script
+. $(dirname "${SCRIPT_PATH}")/run.sh
+
+exit 0
---
+++ examples/asplos25/run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+ulimit -v unlimited
+
+## torch dist.
+export MASTER_ADDR=$(echo $UNWRAPPED_NODELIST | awk '{print $1}')
+export MASTER_PORT=6000
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+
+DISTRIBUTED_ARGS="
+    --tensor-model-parallel-size 1 \
+    --pipeline-model-parallel-size $NP \
+    --distributed-backend nccl \
+    --overlap-p2p-communication \
+    --master-addr $MASTER_ADDR \
+    --master-port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --no-initialization \
+    --untie-embeddings-and-output-weights \
+    --sequence-parallel \
+    --num-layers $LAYER \
+    --hidden-size $HIDDEN \
+    --num-attention-heads $HEAD \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
+    --micro-batch-size $MBS \
+    --global-batch-size $(( $MBS * $NP )) \
+    --lr 0.00015 \
+    --train-iters $TRAIN_ITER \
+    --log-interval $LOG_ITER \
+    --eval-iters $EVAL_ITER \
+    --lr-decay-iters 320000 \
+    --lr-decay-style cosine \
+    --min-lr 1.0e-5 \
+    --weight-decay 1e-2 \
+    --lr-warmup-fraction .01 \
+    --clip-grad 0.0 \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --no-gradient-accumulation-fusion
+"
+
+DATA_ARGS="
+    --data-path $DATA_PATH \
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    --data-impl mmap \
+    --split 949,50,1
+"
+
+EXEC_CMD="python ${MEGATRON_PATH}/pretrain_gpt.py ${EXTRA_ARGS} ${DISTRIBUTED_ARGS} ${GPT_ARGS} ${DATA_ARGS}"
+
+if [ ${NSYS_ENABLE} == "YES" ]; then
+    EXEC_CMD="${NSYS} profile -t cuda,nvtx -o ${NSYS_OUTPUT}_%q{OMPI_COMM_WORLD_RANK} --force-overwrite true ${EXEC_CMD}"
+fi
+
+${MPIRUN} -np $NP -host $HOSTS $MPI_OPTIONS ${EXEC_CMD}
