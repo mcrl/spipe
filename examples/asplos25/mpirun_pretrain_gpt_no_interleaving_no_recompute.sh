66b028f31e1a54be5363861fd8a8efafef2a6c6a
gajagajago <gajagajago@naver.com>
2024-04-17 08:29:37 +0000

    .

---
+++ examples/asplos25/mpirun_pretrain_gpt_no_interleaving_no_recompute.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -J no-interleaving
+#SBATCH --mincpus=4
+#SBATCH --mem=0
+#SBATCH --exclusive
+
+if [ -n "${SLURM_JOB_ID:-}" ] ; then
+    SCRIPT_PATH=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')
+else
+    SCRIPT_PATH=$(realpath "$0")
+fi
+
+# Configuration for custom env
+JOB_TYPE="no-interleaving"
+. $(dirname "${SCRIPT_PATH}")/config.sh
+
+# Configuration for mobius-recompute training
+EXTRA_ARGS="
+    --megatron-mpi
+"
+
+# Run script
+. $(dirname "${SCRIPT_PATH}")/run.sh
+
+exit 0
