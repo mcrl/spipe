9a6adaab695dc957dacf3ac68016f021da204cef
ttochi <jjiny1127@gmail.com>
2024-10-23 16:23:01 +0900

    Remove unused parameter

--- csrc/spiral_cpu_adam/cpu_adam.hpp
+++ csrc/spiral_cpu_adam/cpu_adam.hpp
@@ -17,9 +17,7 @@ typedef unsigned short ds_half_precision_t;
 
 #define STEP(SPAN)                                                             \
   void Step_##SPAN(float* _params, float* grads, float* _exp_avg,              \
-                   float* _exp_avg_sq, size_t _param_size,                     \
-                   ds_half_precision_t* dev_param = nullptr,                   \
-                   bool half_precision = false);
+                   float* _exp_avg_sq, size_t _param_size);
 
 #define ROLLBACK(SPAN)                                                         \
   void Rollback_##SPAN(float* _params, float* grads, float* _exp_avg,          \
@@ -47,9 +45,7 @@ public:
                 float* grads,
                 float* _exp_avg,
                 float* _exp_avg_sq,
-                size_t param_size,
-                ds_half_precision_t* dev_param = nullptr,
-                bool half_precision = false);
+                size_t param_size);
   
   template <int span>
   void Rollback_AVX(size_t* rounded_size,
@@ -123,16 +119,14 @@ private:
 #if defined(__AVX512__) or defined(__AVX256__)
 template <int span>
 void SpiralAdamOptimizer::Step_AVX(size_t* rounded_size,
-                                     float* _params,
-                                     float* grads,
-                                     float* _exp_avg,
-                                     float* _exp_avg_sq,
-                                     size_t _param_size,
-                                     ds_half_precision_t* dev_params,
-                                     bool half_precision)
+                                   float* _params,
+                                   float* grads,
+                                   float* _exp_avg,
+                                   float* _exp_avg_sq,
+                                   size_t _param_size)
 {
   size_t new_rounded_size = 0;
-  int rshft = half_precision ? 1 : 0;
+  int rshft = 0;
 
   AVX_Data betta1_4;
   betta1_4.data = SIMD_SET(_betta1);
@@ -170,7 +164,7 @@ void SpiralAdamOptimizer::Step_AVX(size_t* rounded_size,
 #pragma omp parallel for
     for (size_t i = t; i < offset; i += SIMD_WIDTH * span) {
       AVX_Data grad_4[span];
-      simd_load<span>(grad_4, grads + (i >> rshft), half_precision);
+      simd_load<span>(grad_4, grads + (i >> rshft), false);
 
       AVX_Data momentum_4[span];
       simd_load<span>(momentum_4, _exp_avg + i, false);
@@ -179,7 +173,7 @@ void SpiralAdamOptimizer::Step_AVX(size_t* rounded_size,
       simd_load<span>(variance_4, _exp_avg_sq + i, false);
 
       AVX_Data param_4[span];
-      simd_load<span>(param_4, _params + (i >> rshft), half_precision);
+      simd_load<span>(param_4, _params + (i >> rshft), false);
 
       if (_weight_decay > 0 && !_adamw_mode) {
         simd_fma<span>(grad_4, param_4, weight_decay4, grad_4);
@@ -200,7 +194,7 @@ void SpiralAdamOptimizer::Step_AVX(size_t* rounded_size,
 
       simd_fma<span>(param_4, grad_4, step_size_4, param_4);
 
-      simd_store<span>(_params + (i >> rshft), param_4, half_precision);
+      simd_store<span>(_params + (i >> rshft), param_4, false);
       simd_store<span>(_exp_avg + i, momentum_4, false);
       simd_store<span>(_exp_avg_sq + i, variance_4, false);
     }
@@ -213,14 +207,11 @@ void SpiralAdamOptimizer::Step_1(float* _params,
                                    float* grads,
                                    float* _exp_avg,
                                    float* _exp_avg_sq,
-                                   size_t _param_size,
-                                   ds_half_precision_t* dev_params,
-                                   bool half_precision)
+                                   size_t _param_size)
 {
   size_t rounded_size = 0;
 #if defined(__AVX512__) or defined(__AVX256__)
-  Step_AVX<1>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size,
-              dev_params, half_precision);
+  Step_AVX<1>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size);
 #endif
   if (_param_size > rounded_size) {
     float betta1_minus1 = 1 - _betta1;
@@ -228,12 +219,6 @@ void SpiralAdamOptimizer::Step_1(float* _params,
 
     float step_size = -1 * _alpha / _bias_correction1;
     float w_decay = -1 * _alpha * _weight_decay;
-    ds_half_precision_t* grads_cast_h;
-    ds_half_precision_t* params_cast_h;
-    if (half_precision) {
-      grads_cast_h = reinterpret_cast<ds_half_precision_t*>(grads);
-      params_cast_h = reinterpret_cast<ds_half_precision_t*>(_params);
-    }
 
     for (size_t t = rounded_size; t < _param_size; t += TILE) {
       size_t copy_size = TILE;
@@ -242,8 +227,8 @@ void SpiralAdamOptimizer::Step_1(float* _params,
       size_t offset = copy_size + t;
 #pragma omp parallel for
       for (size_t k = t; k < offset; k++) {
-        float grad = half_precision ? (float)grads_cast_h[k] : grads[k];
-        float param = half_precision ? (float)params_cast_h[k] : _params[k];
+        float grad = grads[k];
+        float param = _params[k];
         float momentum = _exp_avg[k];
         float variance = _exp_avg_sq[k];
         if (_weight_decay > 0 && !_adamw_mode) {
@@ -263,10 +248,8 @@ void SpiralAdamOptimizer::Step_1(float* _params,
           param += w_decay * param;
         }
         param = grad * step_size + param;
-        if (half_precision)
-          params_cast_h[k] = (ds_half_precision_t)param;
-        else
-          _params[k] = param;
+
+        _params[k] = param;
         _exp_avg[k] = momentum;
         _exp_avg_sq[k] = variance;
       }
@@ -278,42 +261,32 @@ void SpiralAdamOptimizer::Step_4(float* _params,
                                    float* grads,
                                    float* _exp_avg,
                                    float* _exp_avg_sq,
-                                   size_t _param_size,
-                                   ds_half_precision_t* dev_params,
-                                   bool half_precision)
+                                   size_t _param_size)
 {
   size_t rounded_size = 0;
 #if defined(__AVX512__) or defined(__AVX256__)
-  Step_AVX<4>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size,
-              dev_params, half_precision);
+  Step_AVX<4>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size);
 #endif
   if (_param_size > rounded_size)
     Step_1((_params + rounded_size), (grads + rounded_size),
            (_exp_avg + rounded_size), (_exp_avg_sq + rounded_size),
-           (_param_size - rounded_size),
-           (dev_params != nullptr ? (dev_params + rounded_size) : dev_params),
-           half_precision);
+           (_param_size - rounded_size));
 }
 
 void SpiralAdamOptimizer::Step_8(float* _params,
                                    float* grads,
                                    float* _exp_avg,
                                    float* _exp_avg_sq,
-                                   size_t _param_size,
-                                   ds_half_precision_t* dev_params,
-                                   bool half_precision)
+                                   size_t _param_size)
 {
   size_t rounded_size = 0;
 #if defined(__AVX512__) or defined(__AVX256__)
-  Step_AVX<8>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size,
-              dev_params, half_precision);
+  Step_AVX<8>(&rounded_size, _params, grads, _exp_avg, _exp_avg_sq, _param_size);
 #endif
   if (_param_size > rounded_size)
     Step_4((_params + rounded_size), (grads + rounded_size),
            (_exp_avg + rounded_size), (_exp_avg_sq + rounded_size),
-           (_param_size - rounded_size),
-           (dev_params != nullptr ? (dev_params + rounded_size) : dev_params),
-           half_precision);
+           (_param_size - rounded_size));
 }
 
 #if defined(__AVX512__) or defined(__AVX256__)
