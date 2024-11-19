9f9c5a08d2ca81b977b0c35c2ae5ca6d24ded8d5
gajagajago <gajagajago@naver.com>
2024-11-19 15:39:11 +0900

    revert args

--- examples/asplos25/args.sh
+++ examples/asplos25/args.sh
@@ -4,17 +4,12 @@
 export MASTER_ADDR=$(echo $UNWRAPPED_NODELIST | awk '{print $1}')
 export MASTER_PORT=$(comm -23 <(seq 10000 65535 | sort) <(ss -tan | awk '{print $4}' | cut -d':' -f2 | grep -v '^\s*$' | sort -u) | shuf -n 1)
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-# export TORCH_NCCL_USE_COMM_NONBLOCKING=1
-# export NCCL_DEBUG=INFO
-# export NCCL_DEBUG_SUBSYS=ALL
-# export TORCH_DISTRIBUTED_DEBUG=INFO
 
 DISTRIBUTED_ARGS="
     --tensor-model-parallel-size 1 \
     --distributed-backend nccl \
     --master-addr $MASTER_ADDR \
-    --master-port $MASTER_PORT \
-    --distributed-timeout-minutes 1
+    --master-port $MASTER_PORT
 "
 if [[ $NO_PIPELINE_PARALLEL -eq 1 ]]; then
     DISTRIBUTED_ARGS="
@@ -33,8 +28,7 @@ DATA_ARGS="
     --vocab-file $VOCAB_FILE \
     --merge-file $MERGE_FILE \
     --data-impl mmap \
-    --split 949,50,1 \
-    --num-workers 0
+    --split 949,50,1
 "
 
 MIXED_PRECISION_ARGS="
