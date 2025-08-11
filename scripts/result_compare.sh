e06c949c344ffb34ee01dd65a12e614569a54c40
ttochi <jjiny1127@gmail.com>
2025-08-12 01:25:46 +0900

    handle OOM

--- scripts/result_compare.sh
+++ scripts/result_compare.sh
@@ -10,21 +10,24 @@ declare -A exp_elapsed
 
 # create key-value for expected
 while IFS=, read -r jobid name cluster model_size mubs mbs nnode seq tflops elapsed gpu_lat; do
-    [[ "$name" == "name" ]] && continue  # 헤더 스킵
+    [[ "$name" == "name" ]] && continue
     key="${name},${cluster},${model_size},${mubs},${mbs},${nnode},${seq}"
     exp_elapsed["$key"]="$elapsed"
 done < "$expected_file"
 
 # compare with actual
 while IFS=, read -r jobid name cluster model_size mubs mbs nnode seq tflops elapsed gpu_lat; do
-    [[ "$name" == "name" ]] && continue  # 헤더 스킵
+    [[ "$name" == "name" ]] && continue
     key="${name},${cluster},${model_size},${mubs},${mbs},${nnode},${seq}"
     exp_val="${exp_elapsed[$key]}"
 
     [[ -z "$exp_val" ]] && continue
-    [[ -z "$exp_val" || -z "$elapsed" ]] && continue
 
-    error_percent=$(awk -v e="$exp_val" -v a="$elapsed" 'BEGIN { printf "%.2f", ((a - e) / e) * 100 }')
+    if [[ "$exp_val" =~ ^[0-9]+([.][0-9]+)?$ && "$elapsed" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+        error_percent=$(awk -v e="$exp_val" -v a="$elapsed" 'BEGIN { printf "%.2f", ((a - e) / e) * 100 }')
+    else
+        error_percent=""
+    fi
 
     echo "$name,$cluster,$model_size,$mubs,$mbs,$nnode,$seq,$exp_val,$elapsed,${error_percent}%" >> "$output_file"
 done < "$actual_file"
